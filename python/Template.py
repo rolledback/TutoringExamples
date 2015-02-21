6580913e4a81ce16f3c4797672be76ed86ab71a2
rolledback <matthew.rayermann@gmail.com>
2015-02-20 23:12:39 -0600

    Created template file.

---
+++ python/Template.py
@@ -0,0 +1,13 @@
+# Created mm/dd/yyyy
+# Demonstrates _______ in Python
+# Run with: Python 2.7 or 3.4
+
+'''
+Template file for Python examples.
+'''
+
+# main function
+def main():
+    print 'Template file.'
+
+main()
